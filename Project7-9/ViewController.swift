c5fb5fc2342fa971b50265188a4075e54ceae0cd
fauzandp <fauzan27dp@gmail.com>
2023-05-02 16:03:29 +0700

    Project7-9 | Hanging Game \n add label answer and guess button

--- Project7-9/ViewController.swift
+++ Project7-9/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentAnswer: UILabel!
+    var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currentAnswer = UILabel()
+        currentAnswer.text = "????????"
+        currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+        currentAnswer.font = UIFont.systemFont(ofSize: 36)
+        currentAnswer.numberOfLines = 1
+        view.addSubview(currentAnswer)
+        
+        submitButton = UIButton(type: .system)
+        submitButton.translatesAutoresizingMaskIntoConstraints = false
+        submitButton.setTitle("Guess", for: .normal)
+        submitButton.setTitleColor(.white, for: .normal)
+        submitButton.backgroundColor = .blue
+        submitButton.titleLabel?.numberOfLines = 1
+        submitButton.titleLabel?.font = UIFont.systemFont(ofSize: 24, weight: .heavy)
+        submitButton.layer.cornerRadius = 20
+        view.addSubview(submitButton)
+        
+        NSLayoutConstraint.activate([
+            // currentAnswer
+            currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentAnswer.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            // submitButton
+            submitButton.widthAnchor.constraint(equalToConstant: 100),
+            submitButton.heightAnchor.constraint(equalToConstant: 44),
+            submitButton.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor, constant: 50),
+            submitButton.centerXAnchor.constraint(equalTo: currentAnswer.centerXAnchor)
+        ])
+           
     }
-
-
+    
 }
 
